2a7dfc494c80f70b613c66dd8b1dea26548bbe68
phantomqi <phantomqi@qq.com>
2022-05-29 18:15:54 +0800

    chore: 更新首页，添加联系方法和首页链接

--- tools/index.py
+++ tools/index.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 #encoding=utf-8
 
-# todo:
-# 1. 以toplevel为根目录
-
 import os, os.path
 from subprocess import run, PIPE
 import time
@@ -19,7 +16,7 @@ def collect_files(parentDir) :
 
         path = os.path.join(parentDir, name)
         if os.path.isdir(path) :
-            if not name.endswith('.assets') :
+            if not name.endswith('.assets') and name not in ['tools'] :
                 for i in collect_files(path) :
                     yield i
             continue
@@ -34,24 +31,21 @@ if __name__ == '__main__' :
     rootDir = run('git rev-parse --show-toplevel'.split(' '), stdout = PIPE).stdout.decode('utf8').rstrip()
     print("rootDir = {}".format(rootDir))
 
-    with open('README.md', 'rt', encoding = 'utf-8') as f :
-        content = f.read()
-        # print(content)
-        i = content.find('---')
-        # print(i)
-        if i > 0 :
-            content = content[:i] + '---\n\n'
+    toc = ''
+    for file in collect_files(rootDir) :
+        relpath = os.path.relpath(file, rootDir)
+        base_name = os.path.splitext(os.path.basename(file))[0]
 
-        for file in collect_files(rootDir) :
-            relpath = os.path.relpath(file, rootDir)
-            base_name = os.path.splitext(os.path.basename(file))[0]
+        mtime = os.path.getmtime(file)
+        localtime = time.localtime(mtime)
+        mtime_text = time.strftime('%Y-%m-%d %H:%M', localtime)
+        print(os.path.relpath(file, rootDir), '最后更新:', mtime_text)
 
-            mtime = os.path.getmtime(file)
-            localtime = time.localtime(mtime)
-            mtime_text = time.strftime('%Y-%m-%d %H:%M', localtime)
-            print(os.path.relpath(file, rootDir), '最后更新:', mtime_text)
+        toc += '- [{}]({}) (最近更新：{})\n'.format(base_name, relpath, mtime_text)
 
-            content += '- [{}]({}) (最近更新：{})\n'.format(base_name, relpath, mtime_text)
+    with open('tools/README-template.md', 'rt', encoding = 'utf-8') as f :
+        template = f.read()
 
+    content = template.replace('[TOC]', toc)
     with open('README.md', 'wt', encoding = 'utf-8') as f :
         f.write(content)
